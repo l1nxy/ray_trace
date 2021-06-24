25fafd5560f6af8f30583b31aaa1048500e8eec8
Yu <l1nxy.zy@gmail.com>
2021-06-24 20:19:20 +0800

    update module

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 
-mod vec3;
-use vec3::Vec3;
+pub mod ray;
+use ray::vec3::*;
+
 use image::{ImageBuffer, RgbImage};
 fn main() {
     let b = 0.25;
@@ -14,9 +15,9 @@ fn main() {
             (b * 256_f64) as u8,
         ])
     });
-    let v = Vec3::new(1.0,2.0,3.0);
-    let x = Vec3::new(1.0,2.0,3.0);
+    let v = Vec3::new(1.0, 2.0, 3.0);
+    let x = Vec3::new(1.0, 2.0, 3.0);
     let s = v + x;
-    println!("{:#?}",s);
+    println!("{:#?}", s);
     image.save("pic.png").expect("save file failed!");
 }
