23170afbf5e3f8fcafcb7d1972b68c8382aab394
l1nxy <l1nxy.zy@gmail.com>
2021-06-29 00:10:50 +0800

    temp commit

--- src/utils.rs
+++ src/utils.rs
@@ -26,22 +26,25 @@ pub fn cal_color_vec(point: Vec3) -> image::Rgb<u8> {
 
 pub fn ray_color(ray: &Ray, world: &HittableList, depth: i32) -> image::Rgb<u8> {
     let mut rec = HitRecord::new();
-    if depth <= 0 {
-        let point = Vec3::default();
-        return cal_color_vec(point);
-    }
-    if world.hit(*ray, 0.0, f64::INFINITY, &mut rec) {
-        let target = rec.p + rec.normal + random_in_unit_sphere();
-        let new_ray_dir = target - rec.p;
-        let new_ray = Ray::new(&rec.p, &new_ray_dir);
-        let mut color: Color = ray_color(&new_ray, world, depth - 1).into();
-        color.r /= 2;
-        color.g /= 2;
-        color.b /= 2;
-        let ret: image::Rgb<u8> = color.into();
-        return ret;
-    }
+    //if depth <= 0 {
+    //let point = Vec3::default();
+    //return cal_color_vec(point);
+    //}
+    //if world.hit(*ray, 0.0, f64::INFINITY, &mut rec) {
+    //let target = rec.p + rec.normal + random_in_unit_sphere();
+    //let new_ray_dir = target - rec.p;
+    //let new_ray = Ray::new(&rec.p, &new_ray_dir);
+    //let mut color: Color = ray_color(&new_ray, world, depth - 1).into();
+    //color.r /= 2;
+    //color.g /= 2;
+    //color.b /= 2;
+    //let ret: image::Rgb<u8> = color.into();
+    //return ret;
+    //}
 
+    if world.hit(*ray, 0.0, f64::INFINITY, &mut rec) {
+        return cal_color_vec((rec.normal + 1.0) * 0.5);
+    }
     let unit_ray = uini_vec3(&ray.dir);
     let t = 0.5 * (unit_ray.y + 1.0);
     let t1 = 1.0 - t;
